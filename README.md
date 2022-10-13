096b356238ed3ea37156ddad397cd7470eda064b
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-10-13 19:31:34 +0200

    Major update to start full lexical data types

--- README.md
+++ README.md
@@ -1,301 +1,34 @@
 # Lexdata
 
 This rust library is designed to produce compact lexical
-representations of various data types. Lexical representations are
-useful because they enable prefix-based indexing strategies which can
-support range queries. For instance, they can be used directly in
-radix trees or in front-coded dictionaries to obtain all elements
-below, above or between some bounds without a scan.
+representations for XSD data types. However other systems, not using
+XSD, may find the library useful as several core data types are
+implemented, namely:
 
-## Roadmap
-
-The roadmap of data types which Lexdata intends to support and which
-are already implemented are:
-
-- [x] Large integers (GMP)
+- [x] Strings
+- [ ] Large Integers
+- [ ] i32
+- [ ] i64
+- [ ] f32
+- [ ] f64
+- [ ] Dates
+- [ ] Date Time
 - [ ] Large Floats
 - [ ] Large Rationals
 
-## Discussion
+Lexical representations are useful because they enable prefix-based
+indexing strategies which can support range queries. For instance,
+they can be used directly in radix trees or in front-coded
+dictionaries to obtain all elements below, above or between some
+bounds without a scan.
 
-More discussion on the strategies used, and how they are implemented
-can be found in our discussion in TerminusDB on [Lexical
-representation of data
-types](https://github.com/terminusdb/terminusdb-store/blob/main/docs/LEXICAL.md).
+## Aspect
 
-## Large Integers
+XSD types can generally be viewed as having a base type and a some
+constraints, which we call aspects. These aspects restrict what kind
+of data can be present. We store the aspect as a byte, in addition to
+the type, for all types aside for DateTime, Date, and Boolean which
+have no aspect.
 
-The functions for conversion of large integers from GMP are
-`convert_mpz_lex` and `convert_lex_mpz`. These take the GMP
-representation which is given by *limbs*, and a *size* which is
-expressed as the number of limbs.  Each limb an order of magnitude of
-the integer in scales of `2^word_size`.
-
-Lexdata stores large integers in two parts. A size part and a number
-part.
-
+```rust
 ```
-| size | number |
-```
-
-A size is represented as follows:
-
-```
-s = sign
-i = continuation
-o = number
-|sioooooo|iooooooo|...|iooooooo|
-```
-
-Positively signed numbers start will start with a size using a 1
-bit. This ensures that they will be lexically after
-negatives. Negative numbers are represented as the twos complement, so
-that they sort "backwards" with larger magnitudes as smaller.
-
-To extend the size, we use a continuation bit `i` which is `1` if
-there are following bytes. This tells us whether there will be another
-segment following. Placing it at the front of the byte ensures that
-this will sort higher than sizes that terminate at this byte. The
-final byte will use a `0`.
-
-```
-sign = positive
-size = 12:
-
-|10001100|
-
-size = 4095
-|11011111|01111111|
-
-```
-
-The number part is reprsented using 8-bit words as *limbs* analogous
-to the mechanism of GMP. The use of bytes increases the compactness
-for small numbers, which is an advantage.
-
-```
-The number part of 12:
-
-|00001100|
-
-The full represenation of 12, inclusing size of 1:
-
-|10000001|00001100|
-
-The full representation of -12, including size of 1:
-
-|01111110|11110011|
-```
-
-When marshalling from GMP, care has to be taken to remove the leading
-zero-bytes of the representation in order to ensure lexical
-comparison.
-
-For example:
-
-```
-A one limb GMP number: 604534244652:
-
-|00000000|00000000|00000000|10001100|11000001|00001100|10000101|00101100|
-
-And it's representation in Lexdata:
-
-
-  terminal size byte
-   |
-pos|  size 5 ______________limbs______________________
-  ||   /|\  /                                         \
- |10000101|10001100|11000001|00001100|10000101|00101100|
-```
-
-## Large Floats
-
-For large floating point numbers we must store an *exponent*, which is
-somewhat similar to the size for a large integer, together with a
-*mantissa*.
-
-The normal form places the decimal point at the first digit. This
-makes it possible to use the exponent to compare lexically and then to
-directly compare the mantissa lexically as well.
-
-The exponent needs to encode *two* signs. One for the sign of the
-number, and one for the sign of the order.
-
-For the mantissa we should *not* encode a size since the length does
-not indicate magnitude. This means we need to use a representation
-with a continuation bit.
-
-However, the continuation bit must be at the *end* of the word, since
-having further digits does not mean it is larger, except for words
-which have no further digits.
-
-We can use the same twos complement trick with the entire
-representation to obtain the negative numbers.
-
-```
-float representation:
-
-| size | mantissa |
-
-size:
-
-  s: sign
-  m: exponent sign
-  e: exponent
-
-|smceeeee|ceeeeeee|...|ceeeeeee|
-
-mantissa:
-  c: continuation
-  n: number
-
-|nnnnnnnc|nnnnnnnc|...|nnnnnnnc|
-
-The representation of 12.3:
-The normal form is 1.23 * 10^1
-
-Has a significand of 123 which is 01111011
-The exponent is 1
-
-  pos exponent
-   |  continuation
-   | /         mantissa
-pos|| size 1 ___| cont
-  |||    |  /    \|
- |11000001|11110110|
-
-```
-
-## Large Rationals
-
-Every rational number has a finite, unique, lexically sortable
-representation preserving the order relation on rationals *based* on
-(but not identical to) a continued fraction expansion. (See [Kornerup,
-Matula](https://www.semanticscholar.org/paper/LCF%3A-A-Lexicographic-Binary-Representation-of-the-Kornerup-Matula/cea51e9517a6b339697cc8b626d8c639cb689f99)
-for details).
-
-The continued fraction expansion has integer coefficients given by the
-[Euclidean
-algorithm](https://en.wikipedia.org/wiki/Euclidean_algorithm) and this
-representation has very remarkable properties.
-
-The continued fraction expansion is given as:
-
-```
-p/q = a_0 + 1 / (a_1 + (1 / ... (1 / a_n)))
-```
-
-We can write this short hand as:
-
-```
-p/q = [ a_0 / a_1 / ... a_n ]
-
-```
-
-However, this expansion has two equivalent representations. One ending
-in a `1`, and one ending with `a_(n-1) - 1`. To obtain a unique
-representation we will need to normalise the sequence.
-
-Typically this is done by chosing one of either ending with a `1`, or
-a number larger than `1`. However to make it lexically sortable we
-will instead choose to make an *even length sequence*, by either
-expansion or contraction of the sequence to its even numbered length.
-
-For instance the continued fraction expansion of `1/3` is given as:
-
-```
-1/3 = [0 / 2 / 1] = 0 + 1 / (2 + (1 / 1))
-1/3 = [0 / 3] = 0 + 1 / 3
-```
-
-We will choose the later representation.
-
-Each prefix of the continued fraction (called a pre-convergent) forms
-a unique *parent* relationship with subsequent elements bounding the
-values which come after the pre-convergent. However, this bounding
-behvaiour alternates in direction bounding from below, then above,
-then below.. .etc.
-
-We will look at the expansion of 3/5 to see how this alternates:
-
-`3/5 = [0/1/1/2]`:
-
-| sequence    | float   | rational | bounded from |
-|-------------|---------|----------|--------------|
-| `[0]`       |  `0`    |  `0`     | below        |
-| `[0/1]`     |  `1`    |  `1/1`   | above        |
-| `[0/1/1]`   |  `0.5`  |  `1/2`   | below        |
-| `[0/1/1/2]` |  `0.6`  |  `3/5`   | exact        |
-
-This suggests that the lexical ordering must take into account the
-alternation present in the sequence of bounds.
-
-We can do this conceptually by altering the sign of the elements in
-the sequence, to get a directly sortable representation. For instance,
-the above even numbered sequence can be written as:
-
-```
-seq(3/5) = [0,-1,1,-2]
-```
-
-Using the property that all sequences have a unique parent which
-bounds it from the appropriate direction, our representation is
-conceptually finished for *positive* rationals.
-
-To get the *negative* rationals, we need to add a sign flag, with `+`
-for positive, and `-` for negative, at the beginning of our sequence,
-and then flip the signs on the remaining sequence for negatives,
-ensuring that larger negatives are sorted "backwards".
-
-```
-lexical_seq(3/5)  = [+, 0,-1, 1,-2]
-lexical_seq(-3/5) = [-,-0, 1,-1, 2]
-```
-
-To represent these as bit sequences we can use tricks similar to our
-represenation of sizes for each element of the sequence. Here only the
-first bit need give a sign, since each subsequent sign is known from
-context. Hence the first segment is identical to our size
-representation for bignums, but subsequent byte representations can be
-as follows:
-
-```
-c = continuation
-o = number bits
-
-|cooo oooo|
-```
-
-Such that a number is represented by sequences of bytes ignoring the
-continuation bit and the continuation bit is set to zero to terminate.
-
-```
-128:
-
-      1    000 0000 =  128
-      |           |
-cont  |termination|
- | ___|__  | _____|
- |/      \ |/      \
-|1000 0001|0000 0000|
-```
-
-As no coefficient may be zero, we can zero terminate the sequence with
-a final zero in our above representation.
-
-For instance, to represent `3/5`, we can write it as:
-
-```
-3/5 = [+,0,-1,1,-2]
-
-       0        -1        1        -2
- sign  |  cont  |   cont  |   cont  |
- |cont |   |    |    |    |    |    |      zero termination
- || ___|_  | ___|__  | ___|__  | ___|__   ____|__
- ||/     \ |/      \ |/      \ |/      \ /       \
-|1000 0000|1111 1110|0000 0001|1111 1101|0000 0000|
-```
-
-Recall, that for negative polarity terms, the continuation bit is also
-inverted, such that `0` denotes further bytes, and `1` no further
-bytes (preserving the appropriate inverted ordering).
--- src/lib.rs
+++ src/lib.rs
@@ -7,246 +7,229 @@
 //! in radix trees or in front-coded dictionaries to obtain all
 //! elements below, above or between some bounds without a scan.
 
-use rug::{Integer,Complete};
-use std::cmp::Ordering;
+use std::str::from_utf8;
 
-/// size_encode takes a vector representing the
-/// order of a number and converts it into a vector of bytes
-///
-/// Format of the first byte is:
-///
-/// 1cxxxxxx
-///
-/// Format of each following byte is:
-///
-/// cxxxxxxx
-///
-/// where c is the chain bit. If zero, then
-/// we are terminal, otherwise there is another size
-/// to come.
-///
-/// The first bit is a sign bit, we will twos complement the
-/// entire result and therby end up with a negative, so it
-/// is always 1 here.
+use rug::Integer;
+//use std::cmp::Ordering;
 
-const TERMINAL : u8 = 0;
-const FIRST_SIGN : u8 = 0b10000000u8;
-const FIRST_TERMINAL : u8 = 0b00000000u8;
-const CONTINUATION : u8 = 0b10000000u8;
-const FIRST_CONTINUATION : u8 = 0b01000000u8;
-const BASE_MASK : u8 = !CONTINUATION;
-const FIRST_MASK : u8 = !(FIRST_SIGN | FIRST_CONTINUATION);
-const FIRST_MAX : u8 = FIRST_CONTINUATION;
+use bytes::Bytes;
 
-fn size_enc(size : usize) -> Vec<u8> {
-    let mut remainder = size;
-    let mut v = vec![];
-    let mut last = true;
-    while remainder > 0 {
-        if remainder >= CONTINUATION as usize {
-            let continued = if last {TERMINAL} else {CONTINUATION};
-            let byte = continued | ((remainder & BASE_MASK as usize) as u8);
-            v.push(byte);
-        }else if remainder >= FIRST_MAX as usize {
-            // special case where we fit in 7 bits but not 6
-            // and we need a zero padded initial byte.
-            let continued = if last {TERMINAL} else {CONTINUATION};
-            let byte = continued | ((remainder & BASE_MASK as usize) as u8);
-            v.push(byte);
-            let byte = FIRST_SIGN | FIRST_CONTINUATION;
-            v.push(byte)
-        }else{
-            let continued = if last {FIRST_TERMINAL} else {FIRST_CONTINUATION};
-            let byte = FIRST_SIGN | continued | ((remainder & FIRST_MASK as usize) as u8);
-            v.push(byte)
-        }
-        remainder = remainder >> 7;
-        last = false;
-    }
-    v.reverse();
-    v
+// We need one of these for each strategy used to store our data
+enum StorageType {
+    String,
+    Int32,
+    Int64,
+    Float32,
+    Float64,
+    BigInt,
+    BigNum,
+    Date,
+    True,
+    False,
 }
 
-fn size_dec(v : &[u8]) -> (bool,usize,usize) {
-    let mut size : usize = 0;
-    let mut sign = true;
-    for i in 0..v.len() {
-        let vi = v[i] as u8;
-        if i == 0 {
-            sign  = if vi != 0 && vi & FIRST_SIGN == 0 { false } else { true };
-            let vi = if sign { vi } else { !vi };
-            let val = (vi & FIRST_MASK) as usize;
-            if vi & FIRST_CONTINUATION == 0 {
-                return (sign,val,i+1)
+// Since XSD requires storage of the constraints on the data,
+// we do this by adding an aspect tag.
+enum Aspect {
+    // Core
+    String,
+    Boolean,
+    Decimal,
+    Integer,
+    // IEEE
+    Double,
+    Float,
+    //Time and Date
+    Date,
+    Time,
+    DateTime,
+    DateTimpStamp,
+    // Recurring and partial dates
+    GYear,
+    GMonth,
+    GDay,
+    GYearMonth,
+    GMonthDay,
+    Duration,
+    YearMonthDuration,
+    DayTimeDuration,
+    // Limited Range Integer Numbers
+    Byte,
+    Short,
+    Int,
+    Long,
+    UnsignedByte,
+    UnsignedShort,
+    UnsignedInt,
+    UnsignedLong,
+    PositiveInteger,
+    NonNegativeInteger,
+    // Encoded binary
+    HexBinary,
+    Base64Binary,
+    // Miscellaneous
+    AnyURI,
+    Language,
+    NormalizedString,
+    Token,
+    NmToken,
+    Name,
+    NCName,
+    NOtation,
+    QName,
+    ID,
+    IdRef,
+    Entity,
+    // RDF
+    XMLLiteral,
+    PlainLiteral,
+    LangString,
+    // RDFS
+    Literal,
+}
+
+enum Value {
+    String(String),
+    Int32(u32),
+    BigNum(Integer),
+    Int64(u64),
+    Float32(f32),
+    Float64(f64),
+    Boolean(bool),
+}
+
+pub fn aspect_storage(aspect: Aspect) -> StorageType {
+    match aspect {
+        Aspect::String => StorageType::String,
+        Aspect::Decimal => StorageType::BigNum,
+        Aspect::Integer => StorageType::BigInt,
+        _ => panic!("Unimplemented aspect"),
+    }
+}
+
+// 6 bits, leaving 2 more to grow
+// surely there is a conversion between enum and ints
+pub fn aspect_byte(a: Aspect) -> u8 {
+    match a {
+        Aspect::String => 0,
+        Aspect::Boolean => 1,
+        Aspect::Decimal => 2,
+        Aspect::Integer => 3,
+        Aspect::Double => 4,
+        Aspect::Float => 5,
+        Aspect::Date => 6,
+        Aspect::Time => 7,
+        Aspect::DateTime => 8,
+        Aspect::DateTimpStamp => 9,
+        Aspect::GYear => 10,
+        Aspect::GMonth => 11,
+        Aspect::GDay => 12,
+        Aspect::GYearMonth => 13,
+        Aspect::GMonthDay => 14,
+        Aspect::Duration => 15,
+        Aspect::YearMonthDuration => 16,
+        Aspect::DayTimeDuration => 17,
+        Aspect::Byte => 18,
+        Aspect::Short => 19,
+        Aspect::Int => 20,
+        Aspect::Long => 21,
+        Aspect::UnsignedByte => 22,
+        Aspect::UnsignedShort => 23,
+        Aspect::UnsignedInt => 24,
+        Aspect::UnsignedLong => 25,
+        Aspect::PositiveInteger => 26,
+        Aspect::NonNegativeInteger => 27,
+        Aspect::HexBinary => 28,
+        Aspect::Base64Binary => 29,
+        Aspect::AnyURI => 30,
+        Aspect::Language => 31,
+        Aspect::NormalizedString => 32,
+        Aspect::Token => 33,
+        Aspect::NmToken => 34,
+        Aspect::Name => 35,
+        Aspect::NCName => 36,
+        Aspect::NOtation => 37,
+        Aspect::QName => 38,
+        Aspect::ID => 39,
+        Aspect::IdRef => 40,
+        Aspect::Entity => 41,
+        Aspect::XMLLiteral => 42,
+        Aspect::PlainLiteral => 43,
+        Aspect::LangString => 44,
+        Aspect::Literal => 45,
+        // Simplify storage to one byte for bools
+        Aspect::True => 46,
+        Aspect::False => 47,
+    }
+}
+
+pub fn byte_aspect(b: u8) -> Aspect {
+    Aspect::String
+}
+
+pub fn value_to_storage(v: Value, a: Aspect) -> Result<Bytes> {
+    match v {
+        Value::String(v) => {
+            let storage = aspect_storage(a);
+            match a {
+                Aspect::String => Bytes::new(v),
+                _ => panic!("Unexpected aspect for value"),
+            },
+        },
+        Value::Int32(_) => todo!(),
+        Value::BigNum(_) => todo!(),
+        Value::Int64(_) => todo!(),
+        Value::Float32(_) => todo!(),
+        Value::Float64(_) => todo!(),
+        Value::Boolean(b) => {
+            if a == Aspect::Boolean{
+                let mut bytes = Bytes::new();
+                match b {
+                    true => {
+                        let byte = aspect_byte(Aspect::True);
+                        bytes.write(byte);
+                    },
+                    false => {
+                        let byte = aspect_byte(Aspect::False);
+                        bytes.write(byte);
+                    }
+                }
+                Ok(bytes)
             }else{
-                size = size + val
-            }
-        }else{
-            let vi = if sign { vi } else { !vi };
-            let val = (vi & BASE_MASK) as usize;
-            if vi & CONTINUATION == 0 {
-                return (sign,size+val,i+1)
-            }else{
-                size = size + val
+                panic!("This should be an error type");
             }
         }
-        size = size << 7;
-    }
-    (sign,size,v.len())
-}
-
-fn negate(v : &mut [u8]) -> () {
-    for i in 0..v.len() {
-        v[i] = !v[i]
     }
 }
 
-pub fn integer_to_lexical(mut z : Integer) -> Vec<u8> {
-    let negative = match z.cmp0() { Ordering::Less => true, _ => false };
-    let size = z.significant_bits();
-    if size == 0 {
-        return vec![FIRST_SIGN]
+pub fn string_from_bytes(bytes) -> Value{
+    let string = from_utf8(bytes).expect("The database should not store strings in non utf8 format");
+    Value::String(string.to_owned())
+}
+
+pub fn storage_to_value(b: Bytes) -> (Value,Aspect) {
+    let bytes = b.iter();
+    let ty_byte = bytes.next().expect("Empty values are disallowed");
+    let aspect = byte_aspect(ty_byte);
+    let ty = aspect_storage(aspect);
+    if aspect == Aspect::True {
+        (Value::Boolean(true),Aspect::Boolean)
+    }else if aspect == Aspect::False {
+        (Value::Boolean(false),Aspect::Boolean)
     }else{
-        let half_bytes = ((size / 7) + 1) as usize;
-        let mut vec = size_enc(half_bytes);
-
-        // +1 is for the zero terminator
-        let mut words = vec![0; half_bytes+1];
-        for i in 0..half_bytes {
-            // Shift left and add 1 to get rid of zeros
-            let word = (((z.clone() & BASE_MASK).to_u32().unwrap() as u8) << 1) + 0b1;
-            z = z >> 7;
-            words[half_bytes - i - 1] = word
+        match ty {
+            StorageType::String => (string_from_bytes(bytes),aspect),
+            StorageType::Int32 => todo!(),
+            StorageType::Int64 => todo!(),
+            StorageType::Float32 => todo!(),
+            StorageType::Float64 => todo!(),
+            StorageType::BigInt => todo!(),
+            StorageType::BigNum => todo!(),
+            StorageType::Date => todo!(),
         }
-        vec.append(&mut words);
-        if negative {
-            negate(&mut vec);
-        }
-        let full_length = vec.len();
-        vec[full_length-1] = 0;
-        return vec
     }
 }
-
-pub fn lexical_to_integer(v : &[u8]) -> Integer {
-    let (sign,size,offset) = size_dec(v);
-    let mut z = Integer::from(0);
-    if size == 0 { return z };
-    for i in offset..size+1 {
-        if i != offset {
-            z = z * Integer::u_pow_u(2,7).complete()
-        }
-        let val = v[i] >> 1; // remove added low bit to avoid zeros
-        let sval = if sign {val} else {!val};
-        z = z + sval
-    }
-    if !sign {
-        z = z * -1
-    }
-    return z
-}
-
-// mpq_to_lexical
-// lexical_to_mpq
-
-// f64_to_lexical
-// lexical_to_f64
-
-// bin_to_lexical
-// lexical_to_bin
 
 #[cfg(test)]
 mod tests {
-    use rug::{Assign, Integer};
-    use crate::{
-        integer_to_lexical,
-        lexical_to_integer
-    };
-
-    #[test]
-    fn find_bytes_and_pad() {
-        let size = 4095; // [0b00001111u8,0b11111111u8];
-        let enc = crate::size_enc(size);
-        assert_eq!(enc, vec![0b11011111u8, 0b01111111u8]);
-
-        let size = 72057594037927935;
-        let enc = crate::size_enc(size);
-        assert_eq!(enc, vec![0b11000000u8, 0b11111111u8, 0b11111111u8,
-                             0b11111111u8, 0b11111111u8, 0b11111111u8,
-                             0b11111111u8, 0b11111111u8, 0b01111111u8 ])
-
-    }
-
-    #[test]
-    fn in_and_out() {
-        let size = 4095; // [0b00001111u8,0b11111111u8];
-        assert_eq!(size, crate::size_dec(&crate::size_enc(size)).1);
-
-        // just a random number
-        let size = 23423423;
-        assert_eq!(size, crate::size_dec(&crate::size_enc(size)).1);
-
-        // boundary case for overflow spillover
-        let size = 72057594037927935;
-        assert_eq!(size, crate::size_dec(&crate::size_enc(size)).1);
-
-        let size = 1;
-        assert_eq!(size, crate::size_dec(&crate::size_enc(size)).1);
-
-        let size = 0;
-        assert_eq!(size, crate::size_dec(&crate::size_enc(size)).1);
-
-    }
-
-    fn int_lex_int(s : &str) -> String {
-        let mut int = Integer::new();
-        int.assign(Integer::parse(s).unwrap());
-        let vec = integer_to_lexical(int);
-        let res = lexical_to_integer(&vec);
-        return res.to_string_radix(10);
-    }
-
-    #[test]
-    fn round_trip() {
-        let decimal = "10";
-        let s = int_lex_int(&decimal);
-        assert_eq!(s,decimal);
-
-        let decimal = "129";
-        let s = int_lex_int(decimal);
-        assert_eq!(s,decimal);
-
-        let decimal = "98765432109876543210";
-        let s = int_lex_int(decimal);
-        assert_eq!(s,decimal)
-    }
-
-    #[test]
-    fn sort_lexicals() {
-        let v = vec!["2342343",
-                     "87292342342342342342342346547768087384729384729",
-                     "0",
-                     "-23423",
-                     "10",
-                     "91",
-                     "-23",
-                     "1",
-                     "9",
-                     "-9802348729234234223423423432456342342342342346547768087384729384729"];
-
-        let mut vecs : Vec<Vec<u8>> = v.iter().map(|s| integer_to_lexical(s.parse::<Integer>().unwrap())).collect();
-        vecs.sort();
-        let res : Vec<String> = vecs.iter().map(|s| lexical_to_integer(s).to_string_radix(10)).collect();
-        assert_eq!(res,
-                   ["-44329827649690278750120583238331105073228301966427397130579212454247",
-                    "-4187393",
-                    "-233",
-                    "0",
-                    "1",
-                    "9",
-                    "10",
-                    "91",
-                    "2342343",
-                    "87292342342342342342342346547768087384729384729"])
-    }
 }
